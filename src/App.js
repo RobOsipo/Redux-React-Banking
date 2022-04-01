7ef3e91bc29eddaf928f8530a3c579118bcb922c
Robert <osipovitchr@gmail.com>
2022-04-01 11:53:03 -0400

    deposit & withdrawl money funtionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import  { actionCreators } from './state/index.js'
+
+
 
 function App() {
 
 const account = useSelector((state) => state.account)
+const dispatch = useDispatch()
 
-console.log(account)
+const { depositMoney, withdrawMoney  } = bindActionCreators(actionCreators, dispatch)
+
+
 
   return (
     <div className="App">
-     
+     <h1>{account}</h1>
+     <button onClick={ depositMoney(1000) }>Deposit Money</button> 
+     <button onClick={ withdrawMoney(200) }>Withdrawl Money</button>
     </div>
   );
 }

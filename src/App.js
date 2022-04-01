749486bd60c9890a5ef0c94631d52c8ff4b3b390
Robert <osipovitchr@gmail.com>
2022-04-01 11:35:00 -0400

    React 18 syntax + action creators

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import { useSelector } from 'react-redux'
 
 function App() {
+
+const account = useSelector((state) => state.account)
+
+console.log(account)
+
   return (
     <div className="App">
      
---
+++ src/state/action-creators/index.js
@@ -0,0 +1,19 @@
+
+
+export const withdrawMoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type: 'WITHDRAW',
+            payload: {}
+        })
+    }
+}
+
+export const depositMoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type: 'DEPOSIT',
+            payload: amount
+        })
+    }
+}
